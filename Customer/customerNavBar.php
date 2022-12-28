0f4f2c7088acded44fa625e3438877016ef4a1dc
rabiatulazmir <95739190+rabiatulazmir@users.noreply.github.com>
2022-12-28 22:34:56 +0800

    Add files via upload

---
+++ Customer/customerNavBar.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name ="viewreport" content="width=device-width, initial-scale =1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="../Style/nav_bar.css">
+
+</head>
+
+
+<body>
+   
+        <div class="navBar">
+                        
+            	<h1 class ="logo">Resort<span>Booking</span></h1>
+           
+                <ul>
+                    <li><a href='customerMain.php'>Profile</a></li>
+                    <li><a href='customerBooking.php'>Booking</a></li>
+                    <li><a href='customerHistory.php'>History</a></li>
+                    <!-- <li><a href='manageResort.php'>Manage Resort</a></li> -->
+                    <li><a href='managePayment.php'>Manage Payment</a></li>
+                    <li><a href='../Login/logout.php'>Logout</a></li>
+                   
+          
+                </ul>
+           
+        </div>
+
+  
+</body>
+</html>
