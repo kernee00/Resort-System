adf0757c3ba5e1fd2541fb1459e140f99f801329
rafidahjohari <121592384+rafidahjohari@users.noreply.github.com>
2023-01-05 20:41:00 +0800

    Add files via upload

---
+++ Customer/addReviewProcess.php
@@ -0,0 +1,55 @@
+<?php
+    session_start();
+    include_once '../connection.php';
+    include_once 'customerNavBar.php';
+
+
+    $user_id = $_SESSION['user_id'];
+ 
+
+      if(isset($_POST['add_rating'])){
+
+    $ratingID = $_POST['Rating ID'];
+    $ratingDateTime = $_POST['Date'];
+    $marksRated = $_POST['Rating'];
+    $comments = $_POST['Comments'];
+    $bookingID = $_POST['Booking ID']
+
+
+    //to prevent mysql injection
+
+    $ratingID = stripcslashes($ratingID);
+    $ratingDateTime = stripcslashes($ratingDateTime);
+    $marksRated = stripcslashes($marksRated);
+    $comments = stripcslashes($comments);
+
+
+$stmt = $conn->prepare("INSERT INTO ratings (ratingID, ratingDateTime, marksRated, comments, bookingID) VALUES (?,?,?,?,?)");
+        $stmt->bind_param("ssssssbs", $ratingID, $ratingDateTime, $marksRated, $comments, $bookingID);
+        $stmt->execute();
+        $success = $stmt->affected_rows;
+        $stmt->close();
+        if($success>0)
+        {
+            echo "<script>alert('Your review is added succesfully!');</script>";
+           echo"<meta http-equiv='refresh' content='0; url=customerHistory.php'/>";
+        }
+        else
+        {
+            echo "<script>alert('Failed to add resort.');</script>";
+            echo"<meta http-equiv='refresh' content='0; url=customerHistory.php'/>";
+        }
+
+
+}
+
+
+
+else {
+
+   echo"Try again.";
+}
+
+
+
+    ?>
