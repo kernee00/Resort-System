8ecdbedbf7ce71d91e8b28c0d27d6aa2719dd8e6
rafidahjohari <121592384+rafidahjohari@users.noreply.github.com>
2023-01-15 07:09:23 +0800

    Update addReviewProcess.php

--- Customer/addReviewProcess.php
+++ Customer/addReviewProcess.php
@@ -10,24 +10,37 @@
       if(isset($_POST['add_rating'])){
 
     $user_id = $_SESSION['user_id'];
-    $ratingID = $_POST['Rating ID'];
-    $ratingDateTime = $_POST['Date'];
-    $marksRated = $_POST['Rating'];
-    $comments = $_POST['Comments'];
-    $bookingID = $_POST['Booking ID'];
+    // $ratingID = $_POST['rating'];
+    $ratingDateTime = date("Y/m/d");
+    $marksRated = $_POST['rating'];
+    $comments = $_POST['comments'];
+    $commentsf = $_POST['commentsf'];
+    $commentsc = $_POST['commentsc'];
+    // $bookingID = $_POST['Booking ID'];
     $role = $_SESSION['role'];
+    
+
 
 
     //to prevent mysql injection
 
-    $ratingID = stripcslashes($ratingID);
+    // $ratingID = stripcslashes($ratingID);
     $ratingDateTime = stripcslashes($ratingDateTime);
     $marksRated = stripcslashes($marksRated);
     $comments = stripcslashes($comments);
 
 
-$stmt = $conn->prepare("INSERT INTO ratings (ratingID, ratingDateTime, marksRated, comments, bookingID) VALUES (?,?,?,?,?)");
-        $stmt->bind_param("ssssssbs", $ratingID, $ratingDateTime, $marksRated, $comments, $bookingID);
+    $books = $conn->prepare("SELECT bookingID FROM bookings WHERE custID = ? LIMIT 1");
+             $books->bind_param('s',$user_id);
+             $books->execute();
+    $resultbooks = $books->get_result();
+    $valuebook = $resultbooks->fetch_array();
+    $finalbook = $valuebook[0];
+    // $row = mysqli_fetch_array($books);
+    
+
+ $stmt= $conn->prepare("INSERT INTO ratings (ratingDateTime, marksRated, comments, commentsf, commentsc, bookingID) VALUES (?,?,?,?,?,?)");
+        $stmt->bind_param("ssssss", $ratingDateTime, $marksRated, $comments, $commentsf, $commentsc, $finalbook);
         $stmt->execute();
         $success = $stmt->affected_rows;
         $stmt->close();
@@ -38,7 +51,7 @@ $stmt = $conn->prepare("INSERT INTO ratings (ratingID, ratingDateTime, marksRate
         }
         else
         {
-            echo "<script>alert('Failed to add resort.');</script>";
+            echo "<script>alert('Failed to add review and ratings.');</script>";
             echo"<meta http-equiv='refresh' content='0; url=customerHistory.php'/>";
         }
 
