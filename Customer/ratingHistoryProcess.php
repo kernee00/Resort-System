059e140706f72f31fbd7e872cbfe30483bac44be
rafidahjohari <121592384+rafidahjohari@users.noreply.github.com>
2023-01-15 07:11:30 +0800

    Add files via upload

---
+++ Customer/ratingHistoryProcess.php
@@ -0,0 +1,45 @@
+<?php
+    include_once '../connection.php';
+    include_once 'customerNavBar.php';
+
+      if(isset($_POST['rating_history'])){
+        
+    $rating_history = $_POST['ratinghis'];
+    
+
+
+
+    //to prevent mysql injection
+
+    // $ratingID = stripcslashes($ratingID);
+    $rating_history = stripcslashes($rating_history);
+   
+
+
+    $books =$conn->prepare("SELECT * FROM ratings WHERE marksRated = ?");
+             $books->bind_param('s',$rating_history);
+             $books->execute();
+    $resultbooks = $books->get_result();
+    if($resultbooks->num_rows>0)
+    {
+      $valuebook = mysqli_fetch_all($resultbooks, MYSQLI_ASSOC);
+      
+    }
+    else{
+      $valuebook = [];
+    }
+
+}
+
+
+
+// else {
+
+//    echo"Try again.";
+// }
+
+
+
+    ?>
+
+    
